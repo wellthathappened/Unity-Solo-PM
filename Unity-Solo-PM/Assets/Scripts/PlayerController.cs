269fc639f7c4193d35b282981955684cf20fbabc
Ian Lewis <33504743+wellthathappened@users.noreply.github.com>
2025-09-11 12:58:01 -0700

    Camera Rotation System

--- Unity-Solo-PM/Assets/Scripts/PlayerController.cs
+++ Unity-Solo-PM/Assets/Scripts/PlayerController.cs
@@ -3,19 +3,43 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    Vector3 cameraOffset = new Vector3(0, .5f, .2f);
+    Vector2 cameraRotation = Vector2.zero;
+    Camera playerCam;
+    InputAction lookAxis;
     Rigidbody rb;
 
     float inputX;
     float inputY;
 
+    public float Xsensitivity = .1f;
+    public float Ysensitivity = .1f;
     public float speed = 5f;
+    public float camRotationLimit = 90;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        playerCam = Camera.main;
+        lookAxis = GetComponent<PlayerInput>().currentActionMap.FindAction("Look");
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
     private void Update()
     {
+        // Camera Handler
+        playerCam.transform.position = transform.position + cameraOffset;
+
+        cameraRotation.x += lookAxis.ReadValue<Vector2>().x * Xsensitivity;
+        cameraRotation.y += lookAxis.ReadValue<Vector2>().y * Ysensitivity;
+
+        cameraRotation.y = Mathf.Clamp(cameraRotation.y, -camRotationLimit, camRotationLimit);
+
+        playerCam.transform.rotation = Quaternion.Euler(-cameraRotation.y, cameraRotation.x, 0);
+        transform.rotation = Quaternion.AngleAxis(cameraRotation.x, Vector3.up);
+
+        // Movement System
         Vector3 tempMove = rb.linearVelocity;
 
         tempMove.x = inputY * speed;
