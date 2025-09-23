2c19d8d78e6af3c0e83093be815a718b24e2d74e
Ian Lewis <33504743+wellthathappened@users.noreply.github.com>
2025-09-23 12:09:25 -0700

    Added weapon system

--- Unity-Solo-PM/Assets/Scripts/PlayerController.cs
+++ Unity-Solo-PM/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,13 @@ public class PlayerController : MonoBehaviour
     Camera playerCam;
     Rigidbody rb;
     Ray jumpRay;
+    Ray interactRay;
+    RaycastHit interactHit;
+    GameObject pickupObj;
+
+    public PlayerInput input;
+    public Transform weaponSlot;
+    public Weapon currentWeapon;
 
     float inputX;
     float inputY;
@@ -14,16 +21,21 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
     public float jumpHeight = 10f;
     public float jumpRayDistance = 1.1f;
+    public float interactDistance = 1f;
 
     public int health = 5;
     public int maxHealth = 5;
 
+    public bool attacking = false;
 
     private void Start()
     {
+        input = GetComponent<PlayerInput>();
+        interactRay = new Ray(transform.position, transform.forward);
         jumpRay = new Ray(transform.position, -transform.up);
         rb = GetComponent<Rigidbody>();
         playerCam = Camera.main;
+        weaponSlot = playerCam.transform.GetChild(0);
 
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -44,6 +56,23 @@ public class PlayerController : MonoBehaviour
         jumpRay.origin = transform.position;
         jumpRay.direction = -transform.up;
 
+        interactRay.origin = playerCam.transform.position;
+        interactRay.direction = playerCam.transform.forward;
+
+        if (Physics.Raycast(interactRay, out interactHit, interactDistance))
+        {
+            if (interactHit.collider.tag == "weapon")
+            {
+                pickupObj = interactHit.collider.gameObject;
+            }
+        }
+        else
+            pickupObj = null;
+
+        if (currentWeapon)
+            if (currentWeapon.holdToAttack && attacking)
+                currentWeapon.fire();
+
         // Movement System
         Vector3 tempMove = rb.linearVelocity;
 
@@ -53,6 +82,44 @@ public class PlayerController : MonoBehaviour
         rb.linearVelocity = (tempMove.x * transform.forward) +
                             (tempMove.y * transform.up) +
                             (tempMove.z * transform.right);
+    }
+    public void Attack(InputAction.CallbackContext context)
+    {
+        if (currentWeapon)
+        {
+            if (currentWeapon.holdToAttack)
+            {
+                if (context.ReadValueAsButton())
+                    attacking = true;
+                else
+                    attacking = false;
+            }
+
+            else if (context.ReadValueAsButton())
+                currentWeapon.fire();
+        }
+    }
+    public void Reload()
+    {
+        if (currentWeapon)
+            currentWeapon.reload();
+    }
+    public void Interact()
+    {
+        if (pickupObj)
+        {
+            if (pickupObj.tag == "weapon")
+                pickupObj.GetComponent<Weapon>().equip(this);
+        }
+        else
+            Reload();
+    }
+    public void DropWeapon()
+    {
+        if (currentWeapon)
+        {
+            currentWeapon.GetComponent<Weapon>().unequip();
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
